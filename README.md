f6b455e895175486a286a1fe6ff2eedee682c1c6
Simon Liang <no.lhr0909@gmail.com>
2020-05-06 22:35:35 +0800

    init project and integrated dgraph grpc client

---
+++ README.md
@@ -0,0 +1,8 @@
+# dGraph Rust Client
+
+Rust Client for dGraph gRPC protocol.
+
+# Motivation
+
+1. Current Rust implementation is outdated and/or not fully featured.
+2. Trying to Address Performance issues in NodeJS clients by integrating with [Neon](https://github.com/neon-bindings/neon)
---
+++ dgraph-grpc-client/src/client.rs
@@ -0,0 +1,27 @@
+pub mod lib;
+
+pub use lib::api::*;
+
+use std::collections::HashMap;
+use std::string::String;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let mut client = dgraph_client::DgraphClient::connect("http://localhost:9080").await?;
+
+  let response = client
+    .query(tonic::Request::new(Request {
+      query: String::from("{ node(func: uid(0x1)) { uid dgraph.type } }"),
+      best_effort: true,
+      read_only: true,
+      commit_now: true,
+      start_ts: 0,
+      vars: HashMap::new(),
+      mutations: Vec::new(),
+    }))
+    .await?;
+
+  println!("response = {:?}", String::from_utf8(response.into_inner().json).unwrap());
+
+  Ok(())
+}
